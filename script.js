7d8fa0e31b7e1fa28a47aae8842a48dda98b1073
Divya <131344430+Divyajoshi2723@users.noreply.github.com>
2026-01-04 22:03:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// const menuBtn = document.getElementById("menuBtn");
+// const sidebar = document.getElementById("sidebar");
+
+// menuBtn.addEventListener("click", () => {
+//   sidebar.classList.toggle("active");
+// });
+function toggleMenu(){
+  document.getElementById("sidebar").classList.toggle("active");
+}
